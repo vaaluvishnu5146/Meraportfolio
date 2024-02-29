1e352288258ad4ee9e928560f88f625dd5824307
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2024-02-29 20:43:06 +0530

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { useRef } from "react";
+
 export default function App() {
-  return <section>React App</section>;
+  const page = useRef(null);
+
+  function handleColorChange(type = "green") {
+    page.current.style.background = type;
+  }
+  return (
+    <section ref={page} className="container" style={{ height: "90vh" }}>
+      <h1>Signup</h1>
+      <button onClick={() => handleColorChange("green")}>Green</button>
+      <button onClick={() => handleColorChange("blue")}>Blue</button>
+      <button onClick={() => handleColorChange("yellow")}>Yellow</button>
+    </section>
+  );
 }
