fe1c4d895788cd7ae2d11be5bac653a5fcce3e70
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2024-03-01 20:17:08 +0530

    Added axios support to blogs context

--- src/Context/BlogContext.jsx
+++ src/Context/BlogContext.jsx
@@ -1,6 +1,7 @@
 // 1. Creating Blog Context
 // 2. Creating BlogContextProvider
 import { createContext, useState, useEffect } from "react";
+import Axios from "axios";
 
 export const BlogContext = createContext({
   blogs: [],
@@ -10,10 +11,17 @@ export default function BlogContextProvider({ children }) {
   const [blogs, setBlogs] = useState();
 
   useEffect(() => {
-    fetch("http://localhost:5173/data.json")
-      .then((response) => response.json())
+    // fetch("http://localhost:5173/data.json")
+    //   .then((response) => response.json())
+    //   .then((result) => {
+    //     setBlogs(result.data);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+    Axios.get("http://localhost:5173/data.json")
       .then((result) => {
-        setBlogs(result.data);
+        setBlogs(result.data.products);
       })
       .catch((error) => {
         console.log(error);
